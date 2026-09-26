909b3ee131c00da9f58e9eecf4ed3006afd7634c
Eswar Chinthakayala <eswarchinthakayala85@gmail.com>
2026-09-26 18:26:03 +0530

    feat: export optical glass foundation

---
+++ components/haloui/foundations/index.ts
@@ -0,0 +1,24 @@
+export { HaloSurface } from "./halo-surface";
+export type {
+  HaloSurfaceProps,
+  HaloSurfaceIntensity,
+  HaloMaterialRecipe,
+  HaloMaterialDensity,
+  HaloSurfaceElevation,
+} from "./halo-surface";
+
+export { HaloOpticalGlass } from "./halo-optical-glass";
+export type {
+  HaloOpticalGlassProps,
+  HaloOpticalMaterial,
+  HaloOpticalShape,
+} from "./halo-optical-glass";
+
+export { HaloRefractionLayer } from "./halo-refraction-layer";
+export { HaloEdge } from "./halo-edge";
+export { HaloGlow } from "./halo-glow";
+export { HaloHighlight } from "./halo-highlight";
+export { HaloNoise } from "./halo-noise";
+export { HaloFocusRing } from "./halo-focus-ring";
+export { HaloPortalSurface } from "./halo-portal-surface";
+export { HaloThemeProvider } from "./halo-theme-provider";
